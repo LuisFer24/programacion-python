e40769e7ff52f164880e8558b00294d12c4bac79
LuisFer24 <luisfer002400@gmail.com>
2016-11-29 22:00:08 -0600

    Entrega de Laboratorio 3
    Ejercicio 1

---
+++ Laboratorio 3/ejercicio1tarea3.py
@@ -0,0 +1,11 @@
+import math as m
+def distancia(x1,y1,x2,y2):
+    x1=m.radians(x1)
+    y1=m.radians(y1)
+    x2=m.radians(x2)
+    y2=m.radians(y2)
+
+    distancia=6371.01*m.acos(m.sin(x1)*m.sin(x2)+m.cos(x1)*m.cos(x2)*m.cos(y1-y2))
+    return distancia
+
+print distancia
