c3c8c24482d0e40e249d104cc18ea41f74b00984
LuisFer24 <luisfer002400@gmail.com>
2016-11-29 21:56:00 -0600

    Laboratorio 2, Entrega
    Ejercicios 1 y 2

---
+++ Laboraorio 2/ejercicio 2 tarea2.py
@@ -0,0 +1,8 @@
+#Luis Fernando Velasco Garcia
+#Ejercicio2
+#para ejecutal el programa se debe poner en python pago(la distancia recorrida) y dará el resultado deseado
+def pago(distancia):
+    banderazo=8.74
+    tarifa=(d/0.25)*1.84
+    total=tarifa+banderazo
+    print "Pagar", total
---
+++ Laboraorio 2/ejrcicio 1 tarea2.py
@@ -0,0 +1,7 @@
+#Luis Fernando Velasco Garcia
+#Ejercicio 1
+#para ejecutar el programa se debe poner hip(poner el valor del cateto uno,poner el valor del cateto dos)
+import math
+def hip(c1,c2):
+    hip=round(math.sqrt(math.pow(c1,2)+math.pow(c2,2)),2)
+    print "Hipotenusa= ", hip
