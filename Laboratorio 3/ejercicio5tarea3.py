15c280947b8cad1946ddd738d13f653ee9e1f419
LuisFer24 <luisfer002400@gmail.com>
2016-12-13 19:07:22 -0600

    Add files via upload

---
+++ Laboratorio 3/ejercicio5tarea3.py
@@ -0,0 +1,8 @@
+#<>
+#Luis Fernando Velasco García
+#Ejercicio 5
+n=raw_input("Nombre: ")
+if n[0]=='a' or n[0]=='e' or n[0]=='i' or n[0]=='o' or n[0]=='u':
+  print "La primera es vocal"
+else:
+  print "La primera es consonante"
